111376781ca440b982547b85f8fa3fd69eb9b98c
Brian Faust <hello@brianfaust.de>
2016-12-04 18:25:23 +0200

    More php 7.1 return types

--- src/Models/PaymentDetail.php
+++ src/Models/PaymentDetail.php
@@ -11,19 +11,11 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Laravel Payment Details.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace BrianFaust\PaymentDetails\Models;
 
-use BrianFaust\Eloquent\Models\Traits\EncryptAttributes;
+use Illuminate\Database\Eloquent\Models\Traits\EncryptAttributes;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class PaymentDetail extends Model
 {
@@ -33,7 +25,7 @@ class PaymentDetail extends Model
 
     protected $casts = ['data' => 'array'];
 
-    public function holder()
+    public function holder(): MorphTo
     {
         return $this->morphTo();
     }
