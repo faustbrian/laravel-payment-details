4df8e2bdb2f8d086d4da6c9e39260be1654e3b47
Brian Faust <hello@brianfaust.de>
2016-12-04 16:25:30 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Models/PaymentDetail.php
+++ src/Models/PaymentDetail.php
@@ -11,10 +11,19 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Laravel Payment Details.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\PaymentDetails\Models;
 
-use Illuminate\Database\Eloquent\Models\Traits\EncryptAttributes;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Models\Traits\EncryptAttributes;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class PaymentDetail extends Model
