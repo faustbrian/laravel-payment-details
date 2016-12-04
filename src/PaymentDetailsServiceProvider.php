f1a707fe19e2a61792eb77b695fe42958cd3ce68
Brian Faust <hello@brianfaust.de>
2016-12-04 12:02:12 +0200

    Fix extending service provider name

--- src/PaymentDetailsServiceProvider.php
+++ src/PaymentDetailsServiceProvider.php
@@ -11,20 +11,11 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Laravel Payment Details.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace BrianFaust\PaymentDetails;
 
-use BrianFaust\ServiceProvider\ServiceProvider;
+use BrianFaust\ServiceProvider\AbstractServiceProvider;
 
-class PaymentDetailsServiceProvider extends ServiceProvider
+class PaymentDetailsServiceProvider extends AbstractServiceProvider
 {
     public function boot(): void
     {
